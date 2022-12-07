2bb181c52527165724c84e4f3120d9d6f3825c83
Brian Noh <noh24@ymail.com>
2022-12-07 00:56:52 -0500

    add tests for static method, debugging with console writeline, dispose method

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -29,6 +35,34 @@ namespace ToDoList.Tests
       newItem.Description = updateDescription;
       string result = newItem.Description;
       Assert.AreEqual(updateDescription, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> {};
+      List<Item> result = Item.GetAll(); // static method returns List<Item>
+
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine($"Output from empty list GetAll test: {thisItem.Description}");
+      }
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "walk the dog";
+      string description02 = "wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> {newItem1, newItem2};
+      List<Item> result = Item.GetAll();
+      
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine($"Output from second GetAll test: {thisItem.Description}");
+      }
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
