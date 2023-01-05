ec7f98798113b9d2cf2aa92d27772dc401782cf0
Brian Noh <noh24@ymail.com>
2023-01-04 20:20:48 -0500

    add splash page with multiple databases

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,19 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
-    public class HomeController : Controller
-    {
+  public class HomeController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-      [HttpGet("/favorite_photos")]
-      public ActionResult FavoritePhotos()
-      {
-        return View();
-      }
+    public HomeController(ToDoListContext db) // ToDoListContext is just a connection to our db
+    {
+      _db = db;
     }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Category[] cats = _db.Categories.ToArray();
+      Item[] items = _db.Items.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("categories", cats);
+      model.Add("items", items);
+      return View(model);
+
+      // This is another option we can do.
+      // List<Category> cats = _db.Categories.ToList();
+      // List<Item> items = _db.Items.ToList();
+      // ViewBag.categories = cats;
+      // ViewBag.items = items;
+      // return View();
+    }
+    [HttpGet("/favorite_photos")]
+    public ActionResult FavoritePhotos()
+    {
+      return View();
+    }
+  }
 }
